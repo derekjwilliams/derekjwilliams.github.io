eccb072b12897a8b753abfa270a4c491a7df8dba
Derek <Derek61@gmail.com>
2015-01-05 07:26:07 -0700

    logout fixes

--- sayhello/scripts/main.js
+++ sayhello/scripts/main.js
@@ -19,8 +19,8 @@
       label.innerHTML = '<img src="' + r.thumbnail + '" /> Hey ' + r.name;
     });
   });
-  $('logout-of-google').on('click', function() {
-    hello( "facebook" ).logout().then( function(){
+  $('#logout-of-google').on('click', function() {
+    hello("google").logout().then( function(){
       alert("Signed out");
     }, function(e){
       alert( "Signed out error:" + e.error.message );
